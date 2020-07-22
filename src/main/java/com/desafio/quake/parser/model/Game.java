fd53e87dd4c8fa91a6a612f1759cd32ed9180420
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-07-22 14:19:44 -0300

    adicionado models

---
+++ src/main/java/com/desafio/quake/parser/model/Game.java
@@ -0,0 +1,73 @@
+package com.desafio.quake.parser.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Game implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "total_kills")
+	private Long totalKills;
+	@OneToMany(cascade = CascadeType.MERGE)
+	private List<Player> players;
+	
+	public Game() {
+		players = new ArrayList<Player>();
+	}
+	
+	public Game(Long id, Long totalKills, List<Player> players) {
+		this.id = id;
+		this.totalKills = totalKills;
+		this.players = players;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getTotalKills() {
+		return totalKills;
+	}
+
+	public void setTotalKills(Long totalKills) {
+		this.totalKills = totalKills;
+	}
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+	
+	public void addKill() {
+		totalKills++;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", totalKills=" + totalKills + ", players=" + players + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/desafio/quake/parser/model/Player.java
@@ -0,0 +1,66 @@
+package com.desafio.quake.parser.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String userName;
+	private Long kills;
+	
+	public Player() {
+		
+	}
+	
+	public Player(Long id, String userName, Long kills) {
+		this.id = id;
+		this.userName = userName;
+		this.kills = kills;
+	}
+	
+	public Player(String userName, Long kills) {
+		this.userName = userName;
+		this.kills = kills;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Long getKills() {
+		return kills;
+	}
+
+	public void setKills(Long kills) {
+		this.kills = kills;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [id=" + id + ", userName=" + userName + ", kills=" + kills + "]";
+	}
+	
+	
+}
